af094127f2dfaecfd75c94447e400a9bbca25577
Facilita Estudante <sprt.eduardo.client@gmail.com>
2026-06-07 18:24:00 -0400

    Create updater.js

---
+++ updater.js
@@ -0,0 +1,44 @@
+{
+  "By_global_updates": "Intervixus Update VX",
+  "version_updater": "1.0",
+  "politic_global": false,
+  "Android": {
+    "app_name": "Intervixus TUBE",
+    "package_id": "com.intervixus.tube",
+    "description": "BUILD teste",
+    "Topic": [
+      "Tópico 1",
+      "tópico 2"
+    ],
+    "version": "4.0",
+    "download_irl": "Github Releases ou Google Drive",
+    "iconUpdate": "URL direta do ícone, ou caminho do navegador (ícone)",
+    "tag": "bet"
+  },
+  "Pc": {
+    "app_name": "Intervixus TUBE for PC",
+    "package_id": "com.intervixus.tube",
+    "description": "BUILD teste",
+    "Topic": [
+      "Tópico 1",
+      "tópico 2"
+    ],
+    "version": "4.0",
+    "download_irl": "Github Releases ou Google Drive",
+    "iconUpdate": "URL direta do ícone, ou caminho do navegador (ícone)",
+    "tag": "bet"
+  },
+  "Tv": {
+    "app_name": "Intervixus TUBE",
+    "package_id": "com.intervixus.tube",
+    "description": "BUILD teste",
+    "Topic": [
+      "Tópico 1",
+      "tópico 2"
+    ],
+    "version": "4.0",
+    "download_irl": "Github Releases ou Google Drive",
+    "iconUpdate": "URL direta do ícone, ou caminho do navegador (ícone)",
+    "tag": "bet"
+  }
+}
